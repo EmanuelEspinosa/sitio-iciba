bd1e37a39e2833a70ccd93b03752113b96f8e961
Emanuel Espinosa <emanuel.espinosa.roberto@gmail.com>
2025-07-03 19:38:11 -0300

    Se desarrollo el segundo y tercer item del slider de presentación correspondiente a index.html Se agrego la lógica correspondiente desarrollada en JS para que el slider cambie automaticamente cada cierto tiempo. Para pantallas <= 900px se refomulo y adapto el diseño del primer slide, de forma que se muestre correctamente en la mayoria de las pantallas pequeñas.

--- assets/js/index-script.js
+++ assets/js/index-script.js
@@ -7,3 +7,58 @@ navlinks.forEach(link => {
     });
 });
 
+
+
+const slider = document.querySelector(".slider .slider_content");
+const items = document.querySelectorAll(".slider .slider_content .item");
+const dots = document.querySelectorAll(".slider .dots li");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+//Declaro las variables que serviran de apoyo
+
+let active = 0;
+let lengthItems = items.length - 1;
+let refreshSlider = setInterval(() => { next.click() }, 7000);
+
+//Declaro funciones y escuchas de click
+
+function reloadSlider() {
+    let checkLeft = items[active].offsetLeft;
+    slider.style.left = -checkLeft + "px";
+
+    let lastActiveDot = document.querySelector(".slider .dots li.active");
+    lastActiveDot.classList.remove("active");
+    dots[active].classList.add("active");
+
+    clearInterval(refreshSlider);
+    refreshSlider = setInterval(() => { next.click() }, 7000);
+}
+
+next.addEventListener("click", () => {
+    if (active + 1 > lengthItems) {
+        active = 0;
+    }
+    else {
+        active = active + 1;
+    }
+    reloadSlider();
+});
+
+prev.addEventListener("click", () => {
+    if (active - 1 < 0) {
+        active = lengthItems;
+    }
+    else {
+        active = active - 1;
+    }
+    reloadSlider();
+})
+
+dots.forEach((li, key) => {
+    li.addEventListener("click", () => {
+        active = key;
+        reloadSlider();
+    });
+});
+
