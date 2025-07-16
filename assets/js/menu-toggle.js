e81ffd3607660ed700cae0c8681e61a9127aa0ca
Emanuel Espinosa <emanuel.espinosa.roberto@gmail.com>
2025-07-16 20:56:05 -0300

    Se realizan correcciones en el menu de navegación de las distintas páginas del sitio.

--- assets/js/menu-toggle.js
+++ assets/js/menu-toggle.js
@@ -1,5 +1,5 @@
 const menuToggle = document.getElementById("check");
-const navlinks = document.querySelectorAll(".navbar a");
+const navlinks = document.querySelectorAll(".navbar .nav-item");
 
 navlinks.forEach(link => {
     link.addEventListener('click', () =>{
